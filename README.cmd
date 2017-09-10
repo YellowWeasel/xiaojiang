2cd8070c85f00ff27ccfb0da87f8cd7c434769ef
唐阳健 <tangyangjian@gmail.com>
2017-09-10 22:47:32 +0800

    add readme

--- README.cmd
+++
@@ -1,7 +0,0 @@
-### 打包说明
-- 将 ssh://git@git.wecheng.com.cn:7999/sdd/service-mobile.git clone 到 app/src/main/assets/service-mobile 目录下
-- 使用 app/release-key.jks 打包，相关信息见 app/build.gradle
-
-### 原生功能部分
-- MQTT Client
-- 扫码
